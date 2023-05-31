1b92acc72c153530d484add9b034420eed31a8bf
how2loveme <kimjinsub01@naver.com>
2023-06-01 00:40:49 +0900

    posts

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,25 +1,38 @@
 import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import {getAllPostIds, getPostData, getSortedPostsData} from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 import { GetStaticProps, GetStaticPaths } from 'next'
 
 export default function Post({
-                                 postData
+                                 allPostsData
                              }: {
-    postData: {
-        title: string
+    allPostsData: {
         date: string
-        contentHtml: string
-    }
+        title: string
+        id: string
+    }[]
 }) {
     return (
         <Layout>
             <Head>
                 <title>{"post"}</title>
             </Head>
-            <p>kungs</p>
+            <ul style={{listStyle: "circle"}}>
+                {allPostsData.map((post, index) => {
+                    return <li key={post.id||index}><a href={`/posts/${post.id}`}>{`${post.title}`}</a>{` / ${post.date}`}</li>
+                })}
+            </ul>
         </Layout>
     )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
 }
