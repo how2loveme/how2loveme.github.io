6560c83084bf21a627d90d85987a1de3423731e4
how2loveme <kimjinsub01@naver.com>
2023-07-11 15:24:21 +0900

    github-markdown-css 적용

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 import { GetStaticPaths, GetStaticProps } from 'next'
+import styles from './github-markdown.module.css'
 
 export default function Post({
   postData,
@@ -18,13 +19,21 @@ export default function Post({
     <Layout post>
       <Head>
         <title>{postData.title}</title>
+        <meta
+          name={'viewport'}
+          content={'width=device-width, initial-scale=1'}
+        />
+        <link rel={'stylesheet'} href={'github-markdown.css'} />
       </Head>
-      <article className="mx-auto max-w-7xl px-6 lg:px-8">
+      <article className={`mx-auto max-w-7xl px-6 lg:px-8`}>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <div
+          className={`${styles['markdown-body']}`}
+          dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+        />
       </article>
     </Layout>
   )
