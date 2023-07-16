e5c50a9ecda6fa1b6cecb9bb9ace632295616028
how2loveme <kimjinsub01@naver.com>
2023-07-17 02:00:09 +0900

    tags

--- pages/edit.tsx
+++ pages/edit.tsx
@@ -1,7 +1,9 @@
 import Layout from '../components/layout'
 import dynamic from 'next/dynamic'
+import { useState } from 'react'
 
 export default function Edit() {
+  const [mode, setMode] = useState<'insert' | 'modify'>('insert')
   const Editor = dynamic(() => import('../components/editor'), {
     ssr: false,
     loading: () => <p>loading...</p>,
@@ -12,7 +14,7 @@ export default function Edit() {
         <div className="space-y-12">
           <div className="border-b border-gray-900/10 pb-12">
             <h2 className="text-base font-semibold leading-7 text-gray-900">
-              편집하기
+              {mode == 'insert' ? '글쓰기' : '편집하기'}
             </h2>
             <p className="mt-1 pb-10 text-sm leading-6 text-gray-600 border-b">
               This information will be displayed publicly so be careful what you
@@ -22,77 +24,96 @@ export default function Edit() {
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="sm:col-span-4">
                 <label
-                  htmlFor="username"
+                  htmlFor="subject"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Title
+                  제목
                 </label>
                 <div className="mt-2">
                   <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                    <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
-                      workcation.com/
-                    </span>
                     <input
                       type="text"
-                      name="username"
-                      id="username"
-                      autoComplete="username"
+                      name="subject"
+                      id="subject"
+                      autoComplete="subject"
                       className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                      placeholder="janesmith"
+                      placeholder=""
                     />
                   </div>
                 </div>
               </div>
               <div className="sm:col-span-3">
                 <label
-                  htmlFor="country"
+                  htmlFor="category"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Category
+                  분류
                 </label>
                 <div className="mt-2">
                   <select
-                    id="country"
-                    name="country"
+                    id="category"
+                    name="category"
                     autoComplete="country-name"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                   >
-                    <option>United States</option>
-                    <option>Canada</option>
-                    <option>Mexico</option>
+                    <option>개발</option>
+                    <option>일상</option>
                   </select>
                 </div>
               </div>
               <div className="sm:col-span-4">
                 <label
-                  htmlFor="username"
+                  htmlFor="tags"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Tags
+                  태그
                 </label>
                 <div className="mt-2">
                   <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                    <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
-                      workcation.com/
-                    </span>
                     <input
                       type="text"
-                      name="username"
-                      id="username"
-                      autoComplete="username"
+                      name="tags"
+                      id="tags"
+                      autoComplete="tags"
                       className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                      placeholder="janesmith"
+                      placeholder=""
                     />
+                  </div>
+                  <div className="flex flex-wrap gap-x-3 gap-y-2 my-2">
+                    <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                      Badge
+                    </span>
+                    <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+                      Badge
+                    </span>
+                    <span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+                      Badge
+                    </span>
+                    <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                      Badge
+                    </span>
+                    <span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+                      Badge
+                    </span>
+                    <span className="inline-flex items-center rounded-md bg-indigo-50 px-2 py-1 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10">
+                      Badge
+                    </span>
+                    <span className="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
+                      Badge
+                    </span>
+                    <span className="inline-flex items-center rounded-md bg-pink-50 px-2 py-1 text-xs font-medium text-pink-700 ring-1 ring-inset ring-pink-700/10">
+                      Badge
+                    </span>
                   </div>
                 </div>
               </div>
 
               <div className="col-span-full">
                 <label
-                  htmlFor="about"
+                  htmlFor="content"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  About
+                  내용
                 </label>
                 <div className="mt-2">
                   <Editor className={'min-h-2000'} />
